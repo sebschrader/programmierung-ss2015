489dbed455776914e2c0e4135909161917f41127
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2015-05-05 16:57:03 +0200

    E03: Fix assignment 4

--- E03/A4.hs
+++ E03/A4.hs
@@ -1,10 +1,12 @@
+module E03.A4 where
+
 -- (a)
 incEntry :: [Int] -> Int -> [Int]
 -- Optional: Check for negative indexes and abort
 incEntry _      n | n < 0 = error "negative index"
 -- Idea: If list is too short, use the built-in replicate function to create a
 -- list of the remaining missing zeros
-incEntry []     n = incEntry (replicate n 0) n
+incEntry []     n = incEntry (replicate (n +1) 0) n
 -- if you don't like replicate you can implement it by hand with as follows:
 --incEntry []     0 = [1]
 --incEntry []     n = 0:incEntry [] (n-1)
@@ -19,14 +21,14 @@ rsum xs = rsum' 0 (reverse xs)
     where
         rsum' n []     = []
         rsum' n (x:xs) = let s = x+n
-                         in s:rsum_foo s xs
+                         in s:rsum' s xs
 
 -- Advanced: Use the built-in scanl function:
 -- http://hackage.haskell.org/package/base-4.8.0.0/docs/Prelude.html#v:scanl
 -- Note: Although they have the same name, the rsum' from the where in rsum has
 -- nothing to do with the rsum' defined below.
 rsum' :: [Int] -> [Int]
-rsum' l = tail (scan (+) 0 (reverse l))
+rsum' l = tail (scanl (+) 0 (reverse l))
 -- point-free: We can also omit the explicit argument and use function
 -- composition:
 --rsum' = tail . scanl (+) 0 . reverse
