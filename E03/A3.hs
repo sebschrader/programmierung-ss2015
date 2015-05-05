406c98163598e34382b3f4fe3f5c356698e93379
Peter Klausing <peter_klausing@wh2.tu-dresden.de>
2015-05-05 15:34:47 +0200

    Added alternative Solution for E03.A3
    found by a Student Tuesday 1. DS

--- E03/A3.hs
+++ E03/A3.hs
@@ -1,6 +1,6 @@
 module E03.A3 where
 
-data Tree = Branch Int Tree Tree | NIL
+data Tree = Branch Int Tree Tree | NIL deriving Show
 
 good_example = (Branch 2 (Branch 1 NIL NIL) NIL) :: Tree
 bad_example = (Branch 1 (Branch 2 NIL NIL) NIL) :: Tree
@@ -16,3 +16,10 @@ isHeap (Branch x tl@(Branch xl rl ll) NIL)
 	= x > xl && isHeap tl
 isHeap (Branch x tl@(Branch xl rl ll) tr@(Branch xr rr lr))
         = x > xl && x > xr && isHeap tl && isHeap tr
+
+isHeap' :: Tree -> Bool
+isHeap' (NIL) = True
+isHeap' (Branch a l r) = (isBigger a l) && (isBigger a r)
+    where isBigger :: Int -> Tree -> Bool
+          isBigger x (NIL) = True
+          isBigger x (Branch a l r) = a < x
