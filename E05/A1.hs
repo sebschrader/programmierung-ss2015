0e6ab19f12012cac1fd6b3e73defdb109927a931
Peter Klausing <peter_klausing@wh2.tu-dresden.de>
2015-05-11 16:59:58 +0200

    E05: Added initial Solutions for E05 without comments

---
+++ E05/A1.hs
@@ -0,0 +1,28 @@
+module E05.A1 where
+
+
+data Tree a = Node a (Tree a) (Tree a) | Leaf a deriving Show
+
+listZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+listZipWith f (xa : xas) (xb : xbs) = (f xa xb) : listZipWith f xas xbs
+listZipWith _ _ _ = []
+
+treeZipWith :: (a -> b -> c) -> Tree a -> Tree b -> Tree c
+treeZipWith f (Leaf xa) (Leaf xb) = Leaf (f xa xb)
+treeZipWith f (Node xa la ra) (Node xb lb rb) = 
+    Node (f xa xb) (treeZipWith f la lb) (treeZipWith f la lb) 
+treeZipWith f (Leaf xa) (Node xb la ra) =
+    Leaf (f xa xb)
+treeZipWith f (Node xa la ra) (Leaf xb) =
+    Leaf (f xa xb)
+    
+class Zippable t where
+    genericZipWith :: (a -> b -> c) -> t a -> t b -> t c
+    genericZip :: t a -> t b -> t (a,b)
+    genericZip = genericZipWith (\ x y -> (x, y))
+
+instance Zippable Tree where
+    genericZipWith = treeZipWith
+
+instance Zippable [] where
+    genericZipWith = listZipWith
