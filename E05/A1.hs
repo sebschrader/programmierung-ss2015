11f97cda4b42874448e78645acb49f804b0ab2d8
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2015-05-15 12:46:24 +0200

    E05: Define Zippable [] in terms of Prelude functions

--- E05/A1.hs
+++ E05/A1.hs
@@ -1,17 +1,24 @@
 module E05.A1 where
 
+-- We have already seen zipWith in the last exercise, were we used it to
+-- implement pleat'. See the Haskell docs for more info:
+-- http://hackage.haskell.org/package/base-4.8.0.0/docs/Prelude.html#v:zipWith
+-- Here's an implementation of zipWith:
+{-
+zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith f (a:as) (b:bs) = f a b : zipWith f as bs
+zipWith _ _      _      = []
+-}
 
 class Zippable t where
     genericZipWith :: (a -> b -> c) -> t a -> t b -> t c
     genericZip :: t a -> t b -> t (a,b)
     genericZip = genericZipWith (,)
 
-listZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-listZipWith f (xa : xas) (xb : xbs) = (f xa xb) : listZipWith f xas xbs
-listZipWith _ _          _          = []
-
+-- Defining an instance of Zippable for the list type is very simple:
 instance Zippable [] where
-    genericZipWith = listZipWith
+    genericZip     = zip
+    genericZipWith = zipWith
 
 
 data Tree a = Node a (Tree a) (Tree a) | Leaf a deriving Show
