cee3201a4a76e24f3872d8a69f913b15bf4357cc
Trolli Schmittlauch <spiollinux@googlemail.com>
2015-07-19 21:33:03 +0200

    fix "Not in scope: x1"
    x1 isn't defined before being assigned with an input value

--- E12/A2b.hs
+++ E12/A2b.hs
@@ -5,5 +5,5 @@ f x1 x2 x3 = if x1 == 1
               then x2
               else f (x1 - 1) x3 ((x2 * x2) + (x3 * x3))
 
-main = do x1 <- readLn x1
+main = do x1 <- readLn
           print (f x1 1 1)
